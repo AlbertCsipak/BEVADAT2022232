678ed48569ead996e3fdee21de38046bcfb7f589
Albi <csipakalbert0@gmail.com>
2023-03-02 17:30:27 +0100

    GYAK01 Done

--- GYAK/GYAK01/GYAK01.py
+++ GYAK/GYAK01/GYAK01.py
@@ -1,22 +1,97 @@
-#Hozz létre egy változót ezzel az értékkel:
-#“Hello World!”
-#Írasd ki a változó értékét egyben!
-#Írasd ki a változó értékét egyesével!
-#Készíts egy függvényt ami kiírja a változó értékét!
-#Készíts egy .py kiterjesztésű fájlt a mappádba és
-#az előző feladatokat írd le oda is
+# %%
+#Create a function that decides if a list contains any odd numbers.
+#return type: bool
+#function name must be: contains_odd
+#input parameters: input_list
 
-helloString = "Hello World!"
+# %%
+evenList = [2,4,6,8]
+oddList = [1,3,5,7]
+mixedList = [1,2,3,4,5,6,7,8,9]
 
-print(helloString)
+# %%
+def contains_odd(input_list):
+    for i in input_list:
+        if i % 2 == 1:
+            return True
+    return False
 
-for i in helloString:
-    print(i)
+# %%
+print(contains_odd(oddList))
+print(contains_odd(evenList))
+print(contains_odd(mixedList))
+
+# %%
+#Create a function that accepts a list of integers, and returns a list of bool.
+#The return list should be a "mask" and indicate whether the list element is odd or not.
+#(return should look like this: [True,False,False,.....])
+#return type: list
+#function name must be: is_odd
+#input parameters: input_list
+
+# %%
+def is_odd(input_list):
+    tmpList = []
+
+    for i in input_list:
+        if i % 2== 0:
+            tmpList.append(False)
+        else:
+            tmpList.append(True)
+    return tmpList
+
+# %%
+print(is_odd(oddList))
+print(is_odd(evenList))
+print(is_odd(mixedList))
+
+# %%
+
+#Create a function that accpects 2 lists of integers and returns their element wise sum. <br>
+#(return should be a list)
+#return type: list
+#function name must be: element_wise_sum
+#input parameters: input_list_1, input_list_2
+
+# %%
+def element_wise_sum(input_list_1, input_list_2):
+    tmpList = []
+    for i in range(0,len(input_list_1),1):
+        tmpList.append(input_list_1[i] + input_list_2[i])
+    return tmpList
+
+# %%
+print(element_wise_sum(evenList, oddList))
+
+# %%
+#Create a function that accepts a dictionary and returns its items as a list of tuples
+#(return should look like this: [(key,value),(key,value),....])
+#return type: list
+#function name must be: dict_to_list
+#input parameters: input_dict
+
+# %%
+person = {
+    'first_name': 'John',
+    'last_name': 'Doe',
+    'age': 30,
+    'address': '123 Main Street',
+    'city': 'New York',
+    'country': 'USA'
+}
+
+# %%
+def dict_to_list(input_dict):
+    outputList = []
+    for key, value in input_dict.items():
+        tmp=key,value
+        outputList.append(tmp)
+    return outputList
+
+# %%
+print(dict_to_list(person))
+
+# %%
+#If all the functions are created convert this notebook into a .py file and push to your repo
 
 
-def helloWorldPrinter(wordToPrint):
-    for i in wordToPrint:
-        print(i)    
-
-helloWorldPrinter(helloString)
-        
